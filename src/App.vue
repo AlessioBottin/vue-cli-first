f4419a279a3686247f292b972ab94151d8e2ff65
AlessioBottin <alessiobottin2002@gmail.com>
2021-12-17 16:10:50 +0100

    bonus

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Title />
+    <Jumbo />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Title from "./components/Title.vue";
+import Jumbo from "./components/Jumbo.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Title,
+    Jumbo,
   },
 };
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Jumbo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="jumbo-container">
+            <h1>Scegli Vue il miglior framework per JavaScript</h1>
+            <img src="../assets/logo.png" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Jumbo",
+}
+</script>
+
+<style scoped>
+.jumbo-container {
+    width: 100%;
+    /* test  */
+    background-color: royalblue;
+    padding: 40px;
+    text-align: left;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    background-image: url(https://img.freepik.com/free-vector/green-curve-abstract-background_53876-99569.jpg?size=626&ext=jpg&ga=GA1.2.1518270500.1634601600);
+    background-size: cover;
+}
+
+
+
+</style>
